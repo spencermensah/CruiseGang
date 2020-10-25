963474064f33810d65d4ffabeb4fdb6f7eb82115
Spencer Yeboah-Mensah <blackaroly@gmail.com>
2020-10-25 20:37:02 +0000

    remove title

--- src/components/Product.js
+++ src/components/Product.js
@@ -62,9 +62,7 @@ class Product extends Component {
       <div className="Product">
         {this.props.product.images.length ? <img className="Product__image" src={variantImage.src} alt={`${this.props.product.title} product shot`}/> : null}
 
-        <h5 className="Product__price">{this.props.product.title}</h5>
-        <h6 className="Product__price">£{variant.price}</h6>
-
+        <h5 className="Product__price">£{variant.price}</h5>
 
         {variantSelectors}
 
